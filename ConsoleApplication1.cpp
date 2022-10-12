b778a27ac05a1e3fddf2a173f6ffcd9b6c3c6a50
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-10-12 22:12:03 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	double y , s;
+	cout << "please enter the first number:";
+	cin >> x;
+	cout << "please enter the second  number:";
+	cin >> y;
+	s=x+y;
+	cout << s;
+}
