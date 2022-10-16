6f152f366b01bf05892b67de8dee262f4293df1a
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-10-16 20:46:29 +0300

    Add files via upload

---
+++ C++ Calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	double y , sum ,sub ,mult ,div ;
+	cout << "please enter the first number:";
+	cin >> x;
+	cout << "please enter the second  number:";
+	cin >> y;
+	if (x>=y) {
+		sub = x - y;
+	}
+	sum=x+y;
+	mult = x * y;
+	if (y!=0) {
+		div = x / y;}
+	cout << "Addition";
+	cout << sum;
+	cout << "Subtraction";
+	cout << sub;
+	cout << "Multiplication";
+	cout << mult;
+	cout << "Division";
+	cout << div;
+}		
