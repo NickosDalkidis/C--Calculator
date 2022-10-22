f5b497a413daa6a801f9794d6d334bbae666abe8
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-10-22 23:58:32 +0300

    Update C++ Calculator.cpp

--- C++ Calculator.cpp
+++ C++ Calculator.cpp
@@ -1,4 +1,4 @@
-#include <iostream>#include <iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
@@ -25,28 +25,3 @@ int main()
 	cout << "'\n' Division  "<< div;
 
 }
-using namespace std;
-int main()
-{
-	int x;
-	double y , sum ,sub ,mult ,div ;
-	cout << "please enter the first number:";
-	cin >> x;
-	cout << "please enter the second  number:";
-	cin >> y;
-	if (x>=y) {
-		sub = x - y;
-	}
-	sum=x+y;
-	mult = x * y;
-	if (y!=0) {
-		div = x / y;}
-	cout << "Addition";
-	cout << sum;
-	cout << "Subtraction";
-	cout << sub;
-	cout << "Multiplication";
-	cout << mult;
-	cout << "Division";
-	cout << div;
-}		
